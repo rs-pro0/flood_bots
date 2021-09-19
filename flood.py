34cdb59502c795d24d8d6e0974609bed688bffbd
rs-pro0 <rostyslav.yabchenko@gmail.com>
2021-09-19 11:54:28 +0300

    first commit

---
+++ flood.py
@@ -0,0 +1,11 @@
+import pyautogui,time
+import pyperclip
+pyautogui.PAUSE=0.05
+def a(b,c):
+	time.sleep(c)
+	while True:
+		pyperclip.copy(b)
+        pyautogui.hotkey("ctrl","v")
+		pyautogui.press("enter")
+if __name__=="__main__":
+	a(input("Flood text:"),int(input("Seconds to start:")))
---
+++ flood_2.py
@@ -0,0 +1,15 @@
+import pyautogui,time
+import pyperclip
+pyautogui.PAUSE=0.05
+def a(b,c,d):
+    time.sleep(c)
+    text=open(b,encoding="utf-8").read()
+    text=text.split("\n")
+    for i in text:
+        if i=="":continue
+        pyperclip.copy(i)
+        pyautogui.hotkey("ctrl","v")
+        pyautogui.press("enter")
+        time.sleep(d)
+if __name__=="__main__":
+    a(input("File of text:"),int(input("Seconds to start:")),float(input("Interval to sleep:")))
