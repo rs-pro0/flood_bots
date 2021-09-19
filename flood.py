4c409ee0b3729d4228c765067c08f641c82205ff
rs-pro0 <rostyslav.yabchenko@gmail.com>
2021-09-19 20:13:35 +0300

    fix indentation errors

--- flood.py
+++ flood.py
@@ -1,11 +1,12 @@
 import pyautogui,time
 import pyperclip
 pyautogui.PAUSE=0.05
-def a(b,c):
+def a(b,c,d):
     time.sleep(c)
     while True:
         pyperclip.copy(b)
         pyautogui.hotkey("ctrl","v")
         pyautogui.press("enter")
+        time.sleep(d)
 if __name__=="__main__":
-    a(input("Flood text:"),int(input("Seconds to start:")))
+    a(input("Flood text:"),int(input("Seconds to start:")),float(input("Interval:")))
